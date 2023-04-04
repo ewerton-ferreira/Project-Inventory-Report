89f799c704e351b8b63e91170057b1367480a535
Ewerton Ferreira <ewertonferreira@msn.com>
2023-04-04 15:21:01 -0300

    feat: Req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, file):
+        oldest_date = min(item["data_de_fabricacao"] for item in file)
+        close_to_expiration = min(
+            (
+                item["data_de_validade"]
+                for item in file
+                if item["data_de_validade"]
+                > datetime.now().strftime("%Y-%m-%d")
+            ),
+            default=None,
+        )
+        more_products = Counter(
+            item["nome_da_empresa"] for item in file
+        ).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {close_to_expiration}\n"
+            f"Empresa com mais produtos: {more_products}"
+        )
