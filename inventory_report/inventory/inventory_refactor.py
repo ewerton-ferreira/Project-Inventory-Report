6aeb08e3ac38767d5fde65454b161121dc309c78
Ewerton Ferreira <ewertonferreira@msn.com>
2023-04-05 19:54:40 -0300

    fix: Req 11

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,17 +1,17 @@
-
+from inventory_report.importer.importer import Importer
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 
 class InventoryRefactor:
-    def __init__(self, importer):
+    def __init__(self, importer: Importer):
         self.importer = importer
         self.data = []
 
-    def import_data(self, path: str, type: str):
+    def import_data(self, path, type):
         inventory_data = self.importer.import_data(path)
-        self.data.extend(inventory_data)
+        self.data = [*self.data, *inventory_data]
 
         if type == "simples":
             return SimpleReport.generate(inventory_data)
