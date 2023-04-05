95f2e23496ee42befa7ae0807da1f2bdd303d1cd
Ewerton Ferreira <ewertonferreira@msn.com>
2023-04-05 20:02:11 -0300

    fix: Refatorado Req 11

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,22 +1,15 @@
-from inventory_report.importer.importer import Importer
-from inventory_report.reports.simple_report import SimpleReport
-from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.inventory.inventory_iterator import InventoryIterator
+from collections.abc import Iterable
 
 
-class InventoryRefactor:
-    def __init__(self, importer: Importer):
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
         self.importer = importer
         self.data = []
 
     def import_data(self, path, type):
         inventory_data = self.importer.import_data(path)
         self.data = [*self.data, *inventory_data]
-
-        if type == "simples":
-            return SimpleReport.generate(inventory_data)
-        if type == "completo":
-            return CompleteReport.generate(inventory_data)
 
     def __iter__(self):
         return InventoryIterator(self.data)
