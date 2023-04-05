ce0abe40f72f52fc87f6616070c55d3ffc17de6b
Ewerton Ferreira <ewertonferreira@msn.com>
2023-04-04 21:55:10 -0300

    feat: Req 08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mockProduct = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01/05/2021",
+        "02/06/2023",
+        "a1gdfH35d",
+        "ao abrigo de luz",
+    )
+
+    assert mockProduct.__repr__() == (
+        "O produto farinha"
+        " fabricado em 01/05/2021"
+        " por Farinini com validade até 02/06/2023"
+        " precisa ser armazenado ao abrigo de luz."
+    )
